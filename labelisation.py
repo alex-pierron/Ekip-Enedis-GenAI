2eb0a6cd3889d257f0acea0c7bf1559672529fca
Biechy <Lucas.S.Biechy@gmx.fr>
2025-02-01 00:20:41 +0100

    ✨ feat(labels): Chat-Template2Prompt
    Change the chat template into prompt

--- labelisation.py
+++ labelisation.py
@@ -1,6 +1,6 @@
 import json
 import boto3 #API AWS 
-
+from botocore.exceptions import ClientError
 
 session = boto3.Session(
     aws_access_key_id=aws_access_key_id,
@@ -11,30 +11,41 @@ bedrock = session.client(service_name="bedrock-runtime")
 
 model_id = "mistral.mistral-large-2402-v1:0" # model ID from Amazon AWS
 
-messages_list = []
+prompt = "I hate you so much, you are the worst person."
 
-text = "Blablabla" # Edenis text
-initial_message = {
-	"role":"user",
-	"content": [
-		{"text": text}
-	],
+formatted_prompt = f"<s>[INST] {prompt} [/INST]"
+
+classification_categories = ['positive', 'negatif']
+
+
+native_request = {
+    "prompt": formatted_prompt,
+    "max_tokens": 512,
+    "temperature": 0.5,
+    "inputSchema": {
+        "json": {
+            "type": "object",
+            "properties": {
+                "sentiment" : {
+                    "type": "string",
+                    "enum": classification_categories
+                }
+            },
+        }
+    },
 }
 
-messages_list.append(initial_message)
-
-response = bedrock.converse(
-	modelId=model_id,
-	messages=messages_list,
-	inferenceConfig={
-		"stopSequences": [ "POSITIF", "NEGATIF" ],
-		"maxTokens": 512,
-		"temperature": 0.5,
-		"topP": 0.9,
-	},
-	system=[{"text":"You are a NLP expert with a PhD. Signal 'POSITIF' if you think that the sentiment analyse is positif, otherwise 'NEGATIF'."}]
-)
+request = json.dumps(native_request)
 
 
-print(response)
-# out = response["additionalModelResponseFields"]["stop_sequence"] #surement pas ça mais équivalent
+try:
+    response = bedrock.invoke_model(modelId=model_id, body=request)
+
+except (ClientError, Exception) as e:
+    print(f"ERROR: Can't invoke '{model_id}'. Reason: {e}")
+    exit(1)
+
+model_response = json.loads(response["body"].read())
+
+response_text = model_response
+print(response_text)

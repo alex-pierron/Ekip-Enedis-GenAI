0f2fc4badfa0c88bf99f5716ea7de53d449f9aa1
Biechy <Lucas.S.Biechy@gmx.fr>
2025-02-01 01:58:44 +0100

    ✨ feat(labels): Sentiment class
    Can map a text to the label POSITIVE, NEUTRAL or NEGATIVE
    
    https://community.aws/content/2hWA16FSt2bIzKs0Z1fgJBwu589/generating-json-with-the-amazon-bedrock-converse-api and https://community.aws/content/2hW5367isgQOkkXLYjp4JB3Pe16/intro-to-tool-use-with-the-amazon-bedrock-converse-api

--- labelisation.py
+++ labelisation.py
@@ -2,6 +2,7 @@ import json
 import boto3 #API AWS 
 from botocore.exceptions import ClientError
 
+
 session = boto3.Session(
     aws_access_key_id=aws_access_key_id,
     aws_secret_access_key=aws_secret_access_key,
@@ -11,41 +12,74 @@ bedrock = session.client(service_name="bedrock-runtime")
 
 model_id = "mistral.mistral-large-2402-v1:0" # model ID from Amazon AWS
 
-prompt = "I hate you so much, you are the worst person."
+content = """
+Dear Acme Investments,
 
-formatted_prompt = f"<s>[INST] {prompt} [/INST]"
+I am writing to compliment one of your customer service representatives, Shirley Scarry. I recently had the pleasure of speaking with Shirley regarding my account deposit. Shirley was extremely helpful and knowledgeable, and went above and beyond to ensure that all of my questions were answered. Shirley also had Robert Herbford join the call, who wasn't quite as helpful. My wife, Clara Bradford, didn't like him at all.
+Shirley's professionalism and expertise were greatly appreciated, and I would be happy to recommend Acme Investments to others based on my experience.
+Sincerely,
 
-classification_categories = ['positive', 'negatif']
+Carson Bradford
+"""
 
+messages = []
 
-native_request = {
-    "prompt": formatted_prompt,
-    "max_tokens": 512,
-    "temperature": 0.5,
-    "inputSchema": {
-        "json": {
-            "type": "object",
-            "properties": {
-                "sentiment" : {
-                    "type": "string",
-                    "enum": classification_categories
-                }
-            },
-        }
-    },
+message = {
+    "role": "user",
+    "content": [
+        { "text": f"You have to use the sentiment_checker tool to classify the sentiment on the content within the <content> tags.\n\n <content>{content}</content>" }
+    ],
 }
 
-request = json.dumps(native_request)
+messages.append(message)
+
+tool_list = [
+    {
+        "toolSpec": {
+            "name": "sentiment_checker",
+            "description": "Get the sentiment of the article.",
+            "inputSchema": {
+                "json": {
+                    "type": "object",
+                    "properties": {
+                        "overall_sentiment": {
+                            "type": "string",
+                            "description": "The article overall sentiment.",
+                            "enum": ["POSITIVE", "NEUTRAL", "NEGATIVE"],
+                        },
+                    },
+                    "required": [
+                        "overall_sentiment",
+                    ]
+                }
+            }
+        }
+    }
+]
+
+response = bedrock.converse(
+	modelId=model_id,
+	messages=messages,
+	inferenceConfig={
+        "maxTokens": 512,
+        "temperature": 0,
+	},
+    toolConfig=
+    {
+        "tools": tool_list,
+        "toolChoice":{"any": {}}
+    }
+)
 
 
-try:
-    response = bedrock.invoke_model(modelId=model_id, body=request)
+response_message = response['output']['message']
 
-except (ClientError, Exception) as e:
-    print(f"ERROR: Can't invoke '{model_id}'. Reason: {e}")
-    exit(1)
+response_content_blocks = response_message['content']
 
-model_response = json.loads(response["body"].read())
+content_block = next((block for block in response_content_blocks if 'toolUse' in block), None)
 
-response_text = model_response
-print(response_text)
+tool_use_block = content_block['toolUse']
+
+tool_result_dict = tool_use_block['input']
+
+print(json.dumps(tool_result_dict, indent=4))
